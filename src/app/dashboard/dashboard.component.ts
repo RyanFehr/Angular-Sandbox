d2749d7949fb9f3d8fb76d86fd1b70a90f0682dd
Ryan Fehr <rfehr@umail.iu.edu>
2017-07-21 05:48:43 -0400

    animated tiles added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  keyframes
+} from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  animations: [
+    trigger('flipState', [
+      state('back', style({
+        transform: 'rotateY(179.9deg)'
+      })),
+      state('front', style({
+        transform: 'rotateY(0)'
+      })),
+      transition('back => front', animate('500ms ease-out')),
+      transition('front => back', animate('500ms ease-in'))
+    ])
+  ]
 })
 export class DashboardComponent implements OnInit {
 
+  tileOneDirection: string = 'front';
+  tileTwoDirection: string = 'front';
+  tileThreeDirection: string = 'front';
+  tileFourDirection: string = 'front';
+
   constructor() { }
+
+  flipTile(tile: number) {
+    switch (tile) {
+      case 1: {
+        this.tileOneDirection = (this.tileOneDirection == 'front') ? 'back' : 'front';
+        break;
+      }
+      case 2: {
+        this.tileTwoDirection = (this.tileTwoDirection == 'front') ? 'back' : 'front';
+        break;
+      }
+      case 3: {
+        this.tileThreeDirection = (this.tileThreeDirection == 'front') ? 'back' : 'front';
+        break;
+      }
+      case 4: {
+        this.tileFourDirection = (this.tileFourDirection == 'front') ? 'back' : 'front';
+        break;
+      }
+    }
+  }
 
   ngOnInit() {
   }
